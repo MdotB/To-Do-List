8873b7ee5462e25eaaa24fdc992e7b32aa80757f
Mike Bell <michaelbell.va@gmail.com>
2018-09-24 15:25:28 -0400

    routes and views

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/routes');
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+app.set('view engine', 'hbs');
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log('server is running');
